8a64bcd374acb4bd53703e88370abf408f5118f8
JPK583 <joseph.p.kelly@sherwin.com>
2025-08-27 10:59:12 -0400

    Moved annotation check to a seperate bash script because it will most likely be reused.

---
+++ .github/workflows/verify_annotation.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ENVIORNMENT=$1
+
+for OBJECT in *.json; do
+    ANNO_PRESENT="$(jq '.properties | has("annotations")' "$OBJECT")"; if [ $ANNO_PRESENT == "true" ]; then
+        ENV_PRESENT="$(jq '.properties.annotations | contains(["$ENVIORNMENT"])' "$OBJECT")"; if [ $ENV_PRESENT == "true" ]; then
+            echo "$OBJECT is approved for $ENVIORNMENT"
+        else
+            echo "$OBJECT is NOT approved for $ENVIORNMENT. It will be ignored."; #mv "$OBJECT" "$OBJECT".ignore
+        fi
+    else
+        echo "$OBJECT is NOT approved for $ENVIORNMENT, and has no annotations. It will be ignored."; #mv "$OBJECT" "$OBJECT".ignore
+    fi
+done
