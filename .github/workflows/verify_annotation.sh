f889f2b7627f583c585a1a3db5cebff9be3bfced
JPK583 <joseph.p.kelly@sherwin.com>
2025-08-28 11:53:25 -0400

    Debug

--- .github/workflows/verify_annotation.sh
+++ .github/workflows/verify_annotation.sh
@@ -2,18 +2,27 @@
 
 ENVIRONMENT=$1
 DIRECTORY=$2
-FEATURE=$3
+#FEATURE=$3
 FEAT_ARRAY=()
 #FEAT_ARRAY+=("New_Path")
 
 echo $DIRECTORY; echo $FEATURE
 
-cd "$DIRECTORY/$FEATURE"; for OBJECT in *.json; do
-    ANNO_PRESENT="$(jq '.properties | has("annotations")' "$OBJECT")"; if [ "$ANNO_PRESENT" == "true" ]; then
-        ENV_PRESENT="$(jq --arg env "$ENVIRONMENT" '.properties.annotations | contains([$env])' "$OBJECT")"; if [ "$ENV_PRESENT" == "true" ]; then
-            FEAT_ARRAY+=("$(pwd)"); break
+cd "$DIRECTORY"
+for FOLDER in *; do
+    cd $FOLDER
+    FEAT=("$(pwd)")
+    for OBJECT in *.json; do
+        ANNO_PRESENT="$(jq '.properties | has("annotations")' "$OBJECT")"
+        if [ "$ANNO_PRESENT" == "true" ]; then
+            ENV_PRESENT="$(jq --arg env "$ENVIRONMENT" '.properties.annotations | contains([$env])' "$OBJECT")"
+            if [ "$ENV_PRESENT" == "true" ]; then
+                FEAT_ARRAY+=$FEAT
+                break
+            fi
         fi
-    fi
+    done
+    cd ..
 done
 
 echo "TEMP FEAT_ARRAY:"
