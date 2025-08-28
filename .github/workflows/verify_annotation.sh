90221b3a7056ba4ac13ab19bb1817b7b51b76102
JPK583 <joseph.p.kelly@sherwin.com>
2025-08-28 10:17:54 -0400

    Debug

--- .github/workflows/verify_annotation.sh
+++ .github/workflows/verify_annotation.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 ENVIORNMENT=$1
-FEATURE=$2
+DIRECTORY=$2
+FEATURE=$3
 FEAT_ARRAY=()
 #FEAT_ARRAY+=("New_Path")
 
 pwd; echo $FEATURE
 
-cd "$FEATURE"; for OBJECT in *.json; do
+cd "$DIRECTORY/$FEATURE"; for OBJECT in *.json; do
     ANNO_PRESENT="$(jq '.properties | has("annotations")' "$OBJECT")"; if [ $ANNO_PRESENT == "true" ]; then
         ENV_PRESENT="$(jq '.properties.annotations | contains(["$ENVIORNMENT"])' "$OBJECT")"; if [ $ENV_PRESENT == "true" ]; then
             FEAT_ARRAY+=(pwd); break
