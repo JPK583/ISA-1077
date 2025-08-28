5421089015e2d8489dffa9b04b2b786fbdb4bc54
JPK583 <joseph.p.kelly@sherwin.com>
2025-08-28 09:54:52 -0400

    Debug
    Changed some names and added a working for loop to list the directories the script found.

--- .github/workflows/verify_annotation.sh
+++ .github/workflows/verify_annotation.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 ENVIORNMENT=$1
-DIRECTORY=$2
-DIR_ARRAY=()
-#DIR_ARRAY+=("New_Path")
+FEATURE=$2
+FEAT_ARRAY=()
+#FEAT_ARRAY+=("New_Path")
 
-pwd; echo $DIRECTORY
+pwd; echo $FEATURE
 
-cd "$DIRECTORY"; for OBJECT in *.json; do
+cd "$FEATURE"; for OBJECT in *.json; do
     ANNO_PRESENT="$(jq '.properties | has("annotations")' "$OBJECT")"; if [ $ANNO_PRESENT == "true" ]; then
         ENV_PRESENT="$(jq '.properties.annotations | contains(["$ENVIORNMENT"])' "$OBJECT")"; if [ $ENV_PRESENT == "true" ]; then
-            DIR_ARRAY+=(pwd); break
+            FEAT_ARRAY+=(pwd); break
         fi
     fi
 done
 
-echo "TEMP DIR_ARRAY:"
+echo "TEMP FEAT_ARRAY:"
 
-for PATH in DIR_ARRAY; do
+for PATH in "${FEAT_ARRAY[@]}"; do
     echo $PATH;
 done
