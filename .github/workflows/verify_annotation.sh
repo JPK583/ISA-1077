eaad66ac9c0290eedf1247bb1a27ac2e858fa2ad
JPK583 <joseph.p.kelly@sherwin.com>
2025-08-28 08:38:21 -0400

    Debug

--- .github/workflows/verify_annotation.sh
+++ .github/workflows/verify_annotation.sh
@@ -6,7 +6,7 @@ DIRECTORY=$2
 #TEMP DEBUG
 echo pwd; echo $ENVIORNMENT; echo $DIRECTORY;
 
-cd "$DIRECTORY/"; for OBJECT in *.json; do
+cd "$DIRECTORY"; for OBJECT in *.json; do
     ANNO_PRESENT="$(jq '.properties | has("annotations")' "$OBJECT")"; if [ $ANNO_PRESENT == "true" ]; then
         ENV_PRESENT="$(jq '.properties.annotations | contains(["$ENVIORNMENT"])' "$OBJECT")"; if [ $ENV_PRESENT == "true" ]; then
             echo "$OBJECT is approved for $ENVIORNMENT"
